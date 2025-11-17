53e7b738e56691965d2c9b99b35c59645b493610
fred-air-x <198084494+fred-air-x@users.noreply.github.com>
2025-11-17 09:39:29 +0100

    docs: add comments on batch script

--- bin/create_project_bash.sh
+++ bin/create_project_bash.sh
@@ -1,19 +1,31 @@
-#!/bin/bash
+# "shebang" (#!/bin/bash), pour indiquer au système d'exploitation que le script doit être lu avec le shell Bash.
+
+#!/bin/bash 
 
 # Création du fichier avec l'utilisateur
+# "echo" affiche un message dans la console à destination de l'utilisateur.
 
 echo "Vous etes actuellement dans le repertoire: $(pwd)"
 
+# "read -p" permet l'interaction avec l'utilisateur par l'affichage d'un message et la lecture de la réponse à cette question et le stockage de cette réponse dans une variable, ici, on définit la variable "cheminCreation".
+
 read -p "Dans quel repertoire voulez-vous creer le projet ? " cheminCreation
+
+# même processus que précédemment pour la variable "nomProjet"
 
 read -p "Entrez le nom du projet : " nomProjet
 
+# la commande mkdir -p créé un répertoire en s'assurant que les dossiers parents existent et que le répertoire ne soit pas déjà existant. Ici nous récupérons les variables créées préalablement et créons les répertoires de la chaines demandées.
+
 mkdir -p "$cheminCreation/$nomProjet"
+
+# "cd" va permettre de se placer sur le chemin demandé. "||" signifiant "ou", donne l'indication à suivre si la commande précédente & échoué. Ici, la console va afficher un message d'erreur avec "echo" et terminer le script avec "exit" avec le code "1" signalant au script qu'une erreur s'est produite.
 
 cd "$cheminCreation/$nomProjet" || { echo "Erreur: Impossible d'acceder au dossier.";exit 1; }
 
 
-# Creation du skeleton
+# Creation du "skeleton"
+# utilisation des commandes "mkdir" et "touch" pour la création des dossiers et fichiers en respectant l'arborescence du cahier des charges.
 
 mkdir -p \
 .well-known \
